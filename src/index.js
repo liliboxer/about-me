2e29becb262961939beaa27055efd5016c55bb8e
Lili Boxer <ninaliboxer@gmail.com>
2019-04-23 15:57:44 -0700

    variables for event listeners

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const maxResponse = document.getElementById('max-answer');
+const countriesResponse = document.getElementById('countries-answer');
+const coastResponse = document.getElementById('coast-answer');
+const funResponse = document.getElementById('fun-answer'); 
+const desertResponse = document.getElementById('desert-answer');
+const quizResult = document.getElementById('quiz-result');
