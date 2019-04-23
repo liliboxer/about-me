6fee5f86e7a5dd0b3ef0e259084ba1317b162097
Lili Boxer <ninaliboxer@gmail.com>
2019-04-23 16:38:00 -0700

    first test passed

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,45 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+function scoreAnswer(maxAnswer, countriesAnswer, coastAnswer, funAnswer, desertAnswer) {
+    let score = 6;
+    return score;
+
+};
+
+test('all answers correct', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const maxAnswer = 'a cat';
+    const countriesAnswer = '9';
+    const coastAnswer = 'no';
+    const funAnswer = 'yes';
+    const desertAnswer = 'yes';
+
+    const expected = 6;
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const score = scoreAnswer(maxAnswer, countriesAnswer, coastAnswer, funAnswer, desertAnswer);
 
     //Assert
-    assert.equal(true, false);
+    assert.equal(score, expected);
+});
+
+test('all answers wrong', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const maxAnswer = 'a cat';
+    const countriesAnswer = '9';
+    const coastAnswer = 'no';
+    const funAnswer = 'yes';
+    const desertAnswer = 'yes';
+
+    const expected = 6;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const score = scoreAnswer(maxAnswer, countriesAnswer, coastAnswer, funAnswer, desertAnswer);
+
+    //Assert
+    assert.equal(score, expected);
 });
